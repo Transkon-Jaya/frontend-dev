cf4f8ed94ea5385766ef5ed01de405803096ed2d
Mukarabin Ali Amu <mukarabinaliamu@gmail.com>
2025-03-16 05:02:23 +0800

    add asset unit
    asset unit

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -22,6 +22,9 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav me-auto">
           <li class="nav-item">
+            <router-link class="nav-link" to="/about">Asset Unit</router-link>
+          </li>
+          <li class="nav-item">
             <router-link class="nav-link" to="/about">About</router-link>
           </li>
           <li class="nav-item">
