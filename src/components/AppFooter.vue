817fda22248fd573020e27803ff1d515b3484c07
Mukarabin Ali Amu <mukarabinaliamu@gmail.com>
2025-03-19 15:13:05 +0800

    update header
    update header

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -18,11 +18,15 @@
     width: 100%;
     background: #f5f5f5;
     color: #161616;
-    text-align: center;
+    text-align: right;
     padding: 10px 0; /* Tambah padding untuk estetika */
     position: fixed;
     bottom: 0;
     left: 0;
   }
+  .footer p {
+  font-size: 11px; /* Bisa sesuaikan dengan kebutuhan */
+}
+
   </style>
   
